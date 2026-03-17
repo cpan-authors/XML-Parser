c644b81534e012af2d9a300a3374ad5ff3c85a1f
Todd Rinaldo <toddr@cpan.org>
2026-03-16 23:42:34 -0600

    Update changelog and version for 2.48_01 dev release
    Update manifest

--- Parser.pm
+++ Parser.pm
@@ -16,7 +16,7 @@ use Carp;
 
 BEGIN {
     require XML::Parser::Expat;
-    $VERSION = '2.47';
+    $VERSION = '2.48_01';
     die "Parser.pm and Expat.pm versions don't match"
       unless $VERSION eq $XML::Parser::Expat::VERSION;
 }
